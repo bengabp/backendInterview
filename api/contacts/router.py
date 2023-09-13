49c66fc9b18e9d74ab820ee54849f4be10b4881f
Deepak Singh <iamdeepaksinghh@gmail.com>
2023-09-13 18:58:39 +0530

    Fixes responses details in path operation decorators

--- api/contacts/router.py
+++ api/contacts/router.py
@@ -1,7 +1,16 @@
 from fastapi import UploadFile
 from fastapi.routing import APIRouter
+from fastapi.responses import FileResponse
+from typing import Union
 
-from config import logger
+from config import logger, HTTPStatus
+from api.contacts.response_schemas import (
+    ErrorResponse,
+    UploadFileResponse,
+    GetContactsByDateResponse,
+    DeleteFileResponse,
+    ContactsFileInDBResponse,
+)
 
 
 contacts_router = APIRouter(prefix="/contacts", tags=["Contacts"])
@@ -11,7 +20,10 @@ contacts_router = APIRouter(prefix="/contacts", tags=["Contacts"])
     "/",
     summary="Upload contacts",
     description="This endpoint takes contacts from an user in form of a CSV file",
-    responses={},
+    responses={
+        HTTPStatus.ACCEPTED: {"model": UploadFileResponse},
+        HTTPStatus.BAD_REQUEST: {"model": ErrorResponse},
+    },
 )
 async def upload_csv(csv_file: UploadFile):
     return {}
@@ -21,7 +33,10 @@ async def upload_csv(csv_file: UploadFile):
     "/",
     summary="Get contacts from date",
     description="This endpoint returns contacts files uploaded on the date or the range specified",
-    responses={},
+    responses={
+        HTTPStatus.OK: {"model": GetContactsByDateResponse},
+        HTTPStatus.BAD_REQUEST: {"model": ErrorResponse},
+    },
 )
 async def get_contacts_by_date(date: str):
     logger.debug(date)
@@ -32,7 +47,10 @@ async def get_contacts_by_date(date: str):
     "/{contacts_file_uid}",
     summary="Get contacts from file uid",
     description="This endpoint returns contacts files or their structured data of the given file uid",
-    responses={},
+    responses={
+        HTTPStatus.OK: {"model": ContactsFileInDBResponse},
+        HTTPStatus.NOT_FOUND: {"model": ErrorResponse},
+    },
 )
 async def get_contacts_by_uid(contacts_file_uid: str, response: str):
     logger.debug(contacts_file_uid)
@@ -44,7 +62,10 @@ async def get_contacts_by_uid(contacts_file_uid: str, response: str):
     "/{contacts_file_uid}",
     summary="Delete contacts from file uid",
     description="This endpoint deletes contacts files of the given file uid",
-    responses={},
+    responses={
+        HTTPStatus.ACCEPTED: {"model": DeleteFileResponse},
+        HTTPStatus.NOT_FOUND: {"model": ErrorResponse},
+    },
 )
 async def delete_contacts(contacts_file_uid: str):
     logger.debug(contacts_file_uid)
