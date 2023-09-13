eaf7a61df4effbcf5373f548bcbb772164a28ec8
Deepak Singh <iamdeepaksinghh@gmail.com>
2023-09-13 17:10:03 +0530

    Adds tags, summary & descriptions for all endpoints

--- api/contacts/router.py
+++ api/contacts/router.py
@@ -3,30 +3,49 @@ from fastapi.routing import APIRouter
 
 from config import logger
 
-contacts_router = APIRouter()
+
+contacts_router = APIRouter(prefix="/contacts", tags=["Contacts"])
 
 
-@contacts_router.post("/contacts")
+@contacts_router.post(
+    "/",
+    summary="Upload contacts",
+    description="This endpoint takes contacts from an user in form of a CSV file",
+    responses={},
+)
 async def upload_csv(csv_file: UploadFile):
-    logger.debug(csv_file.content_type)
-    logger.debug(csv_file.filename)
     return {}
 
 
-@contacts_router.get("/contacts")
+@contacts_router.get(
+    "/",
+    summary="Get contacts from date",
+    description="This endpoint returns contacts files uploaded on the date or the range specified",
+    responses={},
+)
 async def get_contacts_by_date(date: str):
     logger.debug(date)
     return {}
 
 
-@contacts_router.get("/contacts/{uid}")
-async def get_contacts_by_uid(uid: str, response: str):
-    logger.debug(uid)
+@contacts_router.get(
+    "/{contacts_file_uid}",
+    summary="Get contacts from file uid",
+    description="This endpoint returns contacts files or their structured data of the given file uid",
+    responses={},
+)
+async def get_contacts_by_uid(contacts_file_uid: str, response: str):
+    logger.debug(contacts_file_uid)
     logger.debug(response)
     return {}
 
 
-@contacts_router.delete("/contacts/{uid}")
-async def delete_contacts(uid: str):
-    logger.debug(uid)
+@contacts_router.delete(
+    "/{contacts_file_uid}",
+    summary="Delete contacts from file uid",
+    description="This endpoint deletes contacts files of the given file uid",
+    responses={},
+)
+async def delete_contacts(contacts_file_uid: str):
+    logger.debug(contacts_file_uid)
     return {}
